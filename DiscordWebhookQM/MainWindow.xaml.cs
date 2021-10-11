10a2bb28dc9be2aeb1c99bec15dfeec522dabdc1
AIFuzi <eduard.li2017.ooo@yandex.ru>
2021-10-11 14:33:24 +0300

    Added Json

--- DiscordWebhookQM/MainWindow.xaml.cs
+++ DiscordWebhookQM/MainWindow.xaml.cs
@@ -9,17 +9,20 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DiscordWebhookQM.Module;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace DiscordWebhookQM
 {
     public partial class MainWindow : Window
-    {
+    { 
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +30,7 @@ namespace DiscordWebhookQM
 
         private void B_SendMessage_Click(object sender, RoutedEventArgs e)
         {
-            SendMessageWebhook("https://discord.com/api/webhooks/897063901315878912/DdD6cveWeq1lX9QBpLRVI5PO1sBSOASXRXut8T9-UlFiWdLylxxOgxMq4pRmhoQwFewB", TB_TextMessage.Text, "");
+            SendMessageWebhook("https://discord.com/api/webhooks/897063901315878912/DdD6cveWeq1lX9QBpLRVI5PO1sBSOASXRXut8T9-UlFiWdLylxxOgxMq4pRmhoQwFewB", fileName, "");
         }
 
         private static void SendMessageWebhook(string Url, string Message, string Username)
@@ -41,6 +44,19 @@ namespace DiscordWebhookQM
                     "content", Message
                 }
             });
+        }
+
+        private void B_OpenFileDir_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult dgRes = openFileDialog.ShowDialog();
+
+            if(dgRes == System.Windows.Forms.DialogResult.OK)
+            {
+                T_FileNae.Text = openFileDialog.FileName;
+            }
         }
     }
 }
